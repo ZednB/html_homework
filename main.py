43f4f145c2f2b1080ee1311a1b98d7bd580c276e
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-02-12 20:10:04 +0300

    Готово

--- main.py
+++ main.py
@@ -310,7 +310,7 @@ class MyServer(BaseHTTPRequestHandler):
                             </div>
                             <div class="mb-3">
                                     <label for="exampleInputPassword1" class="form-label">Email</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1">
+                                    <input type="email" class="form-control" id="exampleInputPassword1">
                             </div>
                             <button type="button" class="btn btn-success">Отправить</button>
                         </div>
